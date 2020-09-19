b62ef528fb10cd8d0c87a7c87adc743724a00fa8
Dan Brown <brown.danny.j@gmail.com>
2020-09-19 11:35:16 -0500

    finally working better

--- number-guessing-game/script.js
+++ number-guessing-game/script.js
@@ -1,44 +1,68 @@
 /**
  * Guess The Number Game
- * DONE: Get user value from input and save it to variable numberGuess
- * DONE: Generate a random number 1 to 100 and save it to variable correctNumber
- * DONE: Console whether the guess is too high, too low, or is correct inside playGame function
- * DONE: Create a function called displayResult to move the logic for if the guess is too high, too low, or correct
- * DONE: Complete the showYouWon, showNumberAbove, showNumberBelow
- * DONE: Use the showYouWon... functions within displayResult to display the correct dialog
- * DONE: Save the guess history in a variable called guess
- * DONE: Display the guess history using displayHistory() function
- * DONE: Use the initGame() function to restart the game
  */
 
 // Variable to store the list of guesses 
 let guesses;
-
 // Variable for store the correct random number 
 let correctNumber;
 
-
 window.onload = function () {
+    initGame()
     document.getElementById("number-submit").addEventListener("click", playGame);
     document.getElementById("restart-game").addEventListener("click", initGame)
 }
 
-/**
- * Functionality for playing the whole game
- */
 function playGame() {
-    // *CODE GOES BELOW HERE *
-    let numberGuess = document.getElementById('number-guess').value;
-    saveGuessHistory(numberGuess);
-    displayHistory();
-    displayResult(numberGuess);
+    let numberGuess = document.getElementById("number-guess").value;
+    saveGuessHistory(numberGuess)
+    displayHistory()
+    displayResult(numberGuess)
 }
 
-/**
- * Show the result for if the guess it too high, too low, or correct
- * HINT: Use if, else if, else statement 
- */
-// *CODE GOES BELOW HERE *
+// Initialize a new game by resetting all values and content on the page
+function initGame() {
+    correctNumber = getRandomNumber();
+    guesses = []
+    displayHistory()
+    resetResultContent()
+}
+
+// Reset the results list display
+function resetResultContent() {
+    document.getElementById("result").innerHTML = "";
+}
+
+// Return random number between 1 and 100
+function getRandomNumber() {
+    /**
+     * Math.random returns a number between 0 and 1,
+     * and that's why we multiply it by 100
+     */
+    return Math.floor((Math.random() * 100) + 1);
+}
+
+// Save the user guess entered from the input
+function saveGuessHistory(guess) {
+    guesses.push(guess);
+}
+
+// Display history in HTML 
+function displayHistory() {
+    let index = guesses.length - 1;
+    let list = "<ul class='list-group'>"
+    while (index >= 0) {
+        list +=
+            "<li class='list-group-item'>" +
+            "You guessed " + guesses[index] +
+            "</li>";
+        index -= 1
+    }
+    list += '</ul>'
+    document.getElementById("history").innerHTML = list;
+}
+
+// Display the result in HTML
 function displayResult(numberGuess) {
     if (numberGuess > correctNumber) {
         showNumberAbove()
@@ -49,76 +73,7 @@ function displayResult(numberGuess) {
     }
 }
 
-
-
-/**
- * Initialize a new game by resetting all values and content on the page
- * HINT: reset the correctNumber, guesses, and HTML content
- */
-function initGame() {
-    // *CODE GOES BELOW HERE *
-    //Reset correctNumber
-    //Reset result Display
-    //Reset Guesses array
-    //Reset Guess History
-    correctNumber = getRandomNumber();
-    document.getElementById("result").innerHTML = "";
-    guesses = [];
-    displayHistory();
-}
-
-/**
- * Reset the HTML content for guess history
- */
-function resetResultContent() {
-    document.getElementById("result").innerHTML = "";
-}
-
-/**
- * Return a random number between 1 and 100
- * HINT: Use Math.random 
- */
-function getRandomNumber() {
-    // *CODE GOES BELOW HERE *
-    const randomNumber = Math.floor(Math.random() * 100) + 1;
-    return randomNumber;
-}
-
-/**
- * Save guess history 
- * HINT: Search Google "append to array in javascript"
- * HINT: Use the guesses variable
- */
-function saveGuessHistory(guess) {
-    // *CODE GOES BELOW HERE *
-    guesses.push(guess);
-}
-
-/**
- * Display guess history to user
- * HTML TO USE:
- * <ul class='list-group'>
- *  <li class='list-group-item'>You guessed {number}</li
- * </ul>
- * HINT: use while loop and string concatentation to create a list of guesses
- */
-function displayHistory() {
-    let index = guesses.length - 1; // TODO
-    let list = "<ul class='list-group'>";
-    // *CODE GOES BELOW HERE *
-    while (index >= 0) {
-        list += "<li class='list-group-item'>" + "You guessed " + guesses[index] + "</li>";
-        index -= 1;
-    }
-    list += '</ul>'
-    document.getElementById("history").innerHTML = list;
-}
-
-
-
-/**
- * Retrieve the dialog based on if the guess is wrong or correct 
- */
+// Retrieve the dialog based on if the guess is wrong or correct 
 function getDialog(dialogType, text) {
     let dialog;
     switch (dialogType) {
@@ -136,36 +91,18 @@ function getDialog(dialogType, text) {
 
 function showYouWon() {
     const text = "Awesome job, you got it!"
-    /**
-     * Retrieve the dialog using the getDialog() function
-     * and save it to variable called dialog
-     * HINT: Use the 'won' and text parameters 
-     */
-    // *CODE GOES BELOW HERE *
-    let dialog = getDialog('won', text);
+    let dialog = getDialog('won', text)
     document.getElementById("result").innerHTML = dialog;
 }
 
 function showNumberAbove() {
     const text = "Your guess is too high!"
-    /**
-     * Retrieve the dialog using the getDialog() function
-     * and save it to variable called dialog
-     * HINT: Use the 'warning' and text parameters 
-     */
-    // *CODE GOES BELOW HERE *
-    let dialog = getDialog('warning', text);
+    let dialog = getDialog('warning', text)
     document.getElementById("result").innerHTML = dialog;
 }
 
 function showNumberBelow() {
     const text = "Your guess is too low!"
-    /**
-     * Retrieve the dialog using the getDialog() function
-     * and save it to variable called dialog
-     * HINT: Use the 'warning' and text parameters 
-     */
-    // *CODE GOES BELOW HERE *
-    let dialog = getDialog('warning', text);
+    let dialog = getDialog('warning', text)
     document.getElementById("result").innerHTML = dialog;
 }
